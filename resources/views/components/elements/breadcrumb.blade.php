8da3b9673f2549a67e773a66f6bfc991d03f6ff6
Pedro Macamero <61735196+PedroMacar@users.noreply.github.com>
2025-06-01 21:51:42 +0200

    feat: add assets

---
+++ resources/views/components/elements/breadcrumb.blade.php
@@ -0,0 +1,10 @@
+<div class="page-header">
+    <div class="page-block">
+        <ul class="breadcrumb">
+            <li class="breadcrumb-item"><a href="../dashboard/index.html">Home</a></li>
+            <li class="breadcrumb-item"><a href="javascript: void(0)">Online Courses</a></li>
+            <li class="breadcrumb-item" aria-current="page">Dashboard</li>
+        </ul>
+        <div class="page-header-title"><h2 class="mb-0">Dashboard</h2></div>
+    </div>
+</div>
---
+++ resources/views/components/layouts/partials/footer.blade.php
@@ -0,0 +1,57 @@
+<footer class="pc-footer">
+    <div class="footer-wrapper container-fluid mx-10">
+        <div class="grid grid-cols-12 gap-1.5">
+            <div class="col-span-12 sm:col-span-6 my-1">
+                <p class="m-0">© Able Pro crafted with ♥ by Team <a href="https://themeforest.net/user/phoenixcoded"
+                        class="text-theme-bodycolor dark:text-themedark-bodycolor hover:text-primary-500 dark:hover:text-primary-500"
+                        target="_blank">Phoenixcoded</a></p>
+            </div>
+            <div class="col-span-12 sm:col-span-6 my-1">
+                <ul
+                    class="mb-0 ltr:sm:text-right rtl:sm:text-left *:text-theme-bodycolor dark:*:text-themedark-bodycolor hover:*:text-primary-500 dark:hover:*:text-primary-500">
+                    <li class="inline-block max-sm:mr-2 sm:ml-2"><a href="../index.html">Home</a></li>
+                    <li class="inline-block max-sm:mr-2 sm:ml-2"><a href="https://phoenixcoded.gitbook.io/able-pro/"
+                            target="_blank">Documentation</a></li>
+                    <li class="inline-block max-sm:mr-2 sm:ml-2"><a href="https://phoenixcoded.authordesk.app/"
+                            target="_blank">Support</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer><!-- Required Js -->
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="../assets/js/plugins/simplebar.min.js"></script>
+<script src="../assets/js/plugins/popper.min.js"></script>
+<script src="../assets/js/plugins/i18next.min.js"></script>
+<script src="../assets/js/plugins/i18nextHttpBackend.min.js"></script>
+<script src="../assets/js/icon/custom-font.js"></script>
+<script src="../assets/js/plugins/feather.min.js"></script>
+<script src="../assets/js/component.js"></script>
+<script src="../assets/js/theme.js"></script>
+<script src="../assets/js/multi-lang.js"></script>
+<script src="../assets/js/script.js"></script>
+
+{{--
+<script>layout_change('false');</script>
+<script>layout_theme_contrast_change('false');</script>
+<script>change_box_container('false');</script>
+<script>layout_caption_change('true');</script>
+<script>layout_rtl_change('false');</script>
+<script>preset_change('preset-1');</script>
+<script>main_layout_change('vertical');</script> --}}
+
+<!-- [Page Specific JS] start --><!-- bootstrap-datepicker -->
+<script src="../assets/js/plugins/datepicker-full.min.js"></script>
+<script src="../assets/js/plugins/apexcharts.min.js"></script>
+<script src="../assets/js/plugins/peity-vanilla.min.js"></script><!-- custom widgets js -->
+<script src="../assets/js/widgets/revenue-sales-chart.js"></script>
+<script src="../assets/js/widgets/invites-goal-chart.js"></script>
+<script src="../assets/js/widgets/course-report-bar-chart.js"></script>
+<script src="../assets/js/widgets/total-revenue-line-1-chart.js"></script>
+<script src="../assets/js/widgets/total-revenue-line-2-chart.js"></script>
+<script src="../assets/js/widgets/student-states-chart.js"></script>
+<script src="../assets/js/widgets/activity-line-chart.js"></script>
+<script src="../assets/js/widgets/widget-calender.js"></script>
+<script src="../assets/js/widgets/visitors-bar-chart.js"></script>
+<script src="../assets/js/widgets/earning-courses-line-chart.js"></script>
+<script src="../assets/js/widgets/table-donut.js"></script><!-- [Page Specific JS] end -->
